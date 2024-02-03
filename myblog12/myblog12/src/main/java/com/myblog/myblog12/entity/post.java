3f69cdc52427fd5f2fa141e02827353b2443a4ff
shahnawaz900 <shahnawazmd900@gmail.com>
2024-02-04 00:03:19 +0530

    127:Create Post Features Entity Class Added

---
+++ myblog12/myblog12/src/main/java/com/myblog/myblog12/entity/post.java
@@ -0,0 +1,24 @@
+package com.myblog.myblog12.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class post {
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+
+
+}
